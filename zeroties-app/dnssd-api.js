4f26107581ff7116d72a2fc78f9be52f2b22882b
Chris Satterfield <chris@satterfield.ca>
2019-03-25 16:40:11 -0700

    implemented wrapper around existing library

--- zeroties-app/dnssd-api.js
+++ zeroties-app/dnssd-api.js
@@ -1,9 +1,32 @@
-function advertise(name, address, callback) {
+const dnssd = require('dnssd');
 
+ads = {};
+
+function stopAdvertising(name, address){
+	try{
+		ads[name + address].stop(false);
+	} catch (err) {
+		error("stop error: service not found")
+	}
 }
 
-function getServices(callback) {
+function advertise(name, address, callback) {
+	var options = {};
+	options.name = name;
+	options.host = address;
+	ads[name + address] = new dnssd.Advertisement(dnssd.tcp('http'), 4321, options);
+	ad.start();
+	response.status = 200;
+	callback(response)
+}
 
+const browser = new dnssd.Browser(dnssd.tcp('http')).start();
+
+function getServices(callback) {
+	response = {};
+	response.services = browser.list();
+	response.status = 200;
+	callback(response);
 }
 
 exports.dnssdapi = {
