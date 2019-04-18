9178fc65c01af1d07f498e38cc360ca2f6fe3f52
Felix Grund <felix.grund@scandio.de>
2019-04-18 11:56:27 -0700

    Fixed issues with browser addon

--- zeroties-browser-addon/zeroties-content-script.js
+++ zeroties-browser-addon/zeroties-content-script.js
@@ -1,4 +1,9 @@
 let script = document.createElement('script');
 console.log("injecting script")
-script.src = chrome.extension.getURL('zeroties-browser-api.js');
+if (chrome && chrome.extension) {
+	script.src = chrome.extension.getURL('zeroties-browser-api.js');
+} else if (browser && browser.extension) {
+	script.src = browser.extension.getURL('zeroties-browser-api.js');
+}
+
 (document.head||document.documentElement).appendChild(script);
